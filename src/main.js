88b19b79324b34d4006bb44c9cf96014b7295c57
李经纶 <lijinglun@lijinglundeMacBook-Pro.local>
2018-05-21 11:51:04 +0800

    Change css

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import './style/index.css';
 import router from './router'
 import axios from 'axios'
 
