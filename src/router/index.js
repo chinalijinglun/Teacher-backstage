433abaec406c6e94aeba63ba628fca1b131238aa
ChaosXu2016 <chaos_hf@163.com>
2018-05-24 13:08:18 +0800

    修改路由结构

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,14 @@ import Router from 'vue-router'
 import Main from '@/components/index'
 import classification from '@/pages/courseManagement/classification'
 
+import course from './course';
+import employee from './employee';
+import order from './order';
+import partner from './partner';
+import price from './price';
+import student from './student';
+import teacher from './teacher';
+
 Vue.use(Router)
 
 export default new Router({
@@ -20,6 +28,48 @@ export default new Router({
       path: '/classification',
       name: 'classification',
       component: classification
-    }
+    },
+    ...course.map(item=>{
+      return {
+        path: '/course'+item.path,
+        ...item
+      }
+    }),
+    ...employee.map(item=>{
+      return {
+        path: '/employee'+item.path,
+        ...item
+      }
+    }),
+    ...order.map(item=>{
+      return {
+        path: '/order'+item.path,
+        ...item
+      }
+    }),
+    ...partner.map(item=>{
+      return {
+        path: '/partner'+item.path,
+        ...item
+      }
+    }),
+    ...price.map(item=>{
+      return {
+        path: '/price'+item.path,
+        ...item
+      }
+    }),
+    ...student.map(item=>{
+      return {
+        path: '/student'+item.path,
+        ...item
+      }
+    }),
+    ...teacher.map(item=>{
+      return {
+        path: '/teacher'+item.path,
+        ...item
+      }
+    })
   ]
 })
