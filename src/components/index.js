c6daf70cca703d823405c4632d14e0c5afee1cfa
ChaosXu2016 <chaos_hf@163.com>
2018-05-26 14:54:48 +0800

    index.js中无需引入mixins

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
 import dateRange from './commons/form/dateRange';
-import dialogContainer from './commons/mixins/dialogContainer';
-import paginationMix from './commons/mixins/paginationMix';
 
 const install = (Vue) => {
   Vue.component(dateRange.name, dateRange);
