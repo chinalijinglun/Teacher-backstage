c329b4b88d3fefb4a4363b317e0f196fedff567e
ChaosXu2016 <chaos_hf@yeah.net>
2018-06-18 13:56:14 +0800

    增加枚举

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -6,4 +6,24 @@ export const VALID_ENUM = {
 export const DELETE_FLAG = {
   IN_FORCE: 'IN_FORCE',
   DELETED: 'DELETED'
+};
+
+export const COURSE_TYPE = {
+  ALL: 1,
+  ONLINE: 2,
+  PUBLIC: 3
+};
+
+export const CLASS_TYPE = {
+  '1v1': '1 v 1',
+  '1V4': '1 v 4',
+  '1VN': '1 v n'
+};
+
+export const PROJECT_TYPE = {
+  ALL: 1,
+  MEI_GAO: 2,
+  XUE_FEN: 3,
+  AP: 4,
+  OTHERS: 5
 }
