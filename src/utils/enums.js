3d48b9692e4fe08244c13bd41e8e59afb05603a9
ChaosXu2016 <chaos_hf@163.com>
2018-06-23 09:40:30 +0800

    教师状态枚举

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -19,3 +19,21 @@ export const CLASS_TYPE = {
   '1 v 4': 2,
   '1 v n': 3
 };
+
+// RECRUIT：新用户，
+// BASIC_INFO：填写基本信息，
+// WAIT_FOR_CHECK:待审核，
+// CHECK_PASS:审核通过(待预约)、
+// CHECK_ERROR:审核未通过，
+// WAIT_FOR_INTERVIEW：已预约(待面试)，
+// INTERVIEW_PASS:面试结果通过(待签约)，
+// INTERVIEW_ERROR:面试结果失败，
+// CONTRACTOR:已签约，
+// WAIT_FOR_TRAIN:待预约培训试讲，
+// TRAIN_PASS:培训试讲结果成功，
+// TRAIN_ERROR:培训试讲结果失败
+// WORKING:在岗，
+// NO_WORK:未在岗
+// INVALID：无效
+export const TEACHER_STATE_ENUM = {
+}
